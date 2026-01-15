e0cdec1f6b7f3091c3027b92ccb9203af9e2dbd2
cetoprca <cetoprca@gmail.com>
2026-01-15 19:35:27 +0100

    añadido errores y respuestas en los controllers

--- controllers/camarero_controller.py
+++ controllers/camarero_controller.py
@@ -1,49 +1,95 @@
 from odoo import http
 from odoo.http import request
+from odoo.http import Response 
 import json
 
 class RestauranteCamareroController(http.Controller):
     @http.route('/api/camareros', auth='public', type='json', methods=['GET'])
     def listCamareros(self, **kw):
-        camareros = request.env['restaurante.camarero'].sudo().search([])
-        
-        jsonFinal = []
-        
-        for camarero in camareros:
-            jsonCamarero = camarero.read(['nombre', 'numero_empleado', 'mesa_ids'])[0]
+        try:
+            camareros = request.env['restaurante.camarero'].sudo().search([])
+            
+            jsonFinal = []
+            
+            for camarero in camareros:
+                jsonCamarero = camarero.read(['nombre', 'numero_empleado', 'mesa_ids'])[0]
 
-            jsonFinal.append(jsonCamarero)
-        
-        return jsonFinal 
+                jsonFinal.append(jsonCamarero)
+            
+            return Response(
+                status=200,
+                content_type='application/json',
+                response=jsonFinal
+            ) 
+
+        except:
+            return Response(
+                status=400,
+                content_type='application/json',
+                response=json.dumps({
+                    "error":"Bad Request",
+                    "message":"Datos invalidos"
+                })
+            )
     
     @http.route('/api/camarero/add', auth='public', type='json', methods=['PUT'])
     def addCamarero(self, **kw):
+            
+        try:    
+            data = json.loads(request.httprequest.data.decode('utf-8'))
+            
+            request.env['restaurante.camarero'].sudo().create(data)
         
-        data = json.loads(request.httprequest.data.decode('utf-8'))
-        
-        request.env['restaurante.camarero'].sudo().create(data)
+        except:
+            return Response(
+                status=400,
+                content_type='application/json',
+                response=json.dumps({
+                    "error":"Bad Request",
+                    "message":"Datos invalidos"
+                })
+            )
         
     
     @http.route('/api/camarero/update', auth='public', type='json', methods=['PATCH'])
     def updateCamarero(self, **kw):
         
-        data = json.loads(request.httprequest.data.decode('utf-8'))
+        try:
+            data = json.loads(request.httprequest.data.decode('utf-8'))
 
-        numero_empleado = data.get('numero_empleado')
-        
-        camarero = request.env['restaurante.camarero'].sudo().search([('numero_empleado', '=', numero_empleado)])
-        
-        data.pop('numero_empleado')
-        
-        camarero.sudo().write(data)
+            numero_empleado = data.get('numero_empleado')
+            
+            camarero = request.env['restaurante.camarero'].sudo().search([('numero_empleado', '=', numero_empleado)])
+            
+            data.pop('numero_empleado')
+            
+            camarero.sudo().write(data)
+        except:
+            return Response(
+                status=400,
+                content_type='application/json',
+                response=json.dumps({
+                    "error":"Bad Request",
+                    "message":"Datos invalidos"
+                })
+            )
 
     @http.route('/api/camarero/delete', auth='public', type='json', methods=['DELETE'])
     def deleteCamarero(self, **kw):
-        data = json.loads(request.httprequest.data.decode('utf-8'))
-        
-        numero_empleado = data.get('numero_empleado')
-        
-        camarero = request.env['restaurante.camarero'].sudo().search([('numero_empleado', '=', numero_empleado)])
-        
-        camarero.sudo().unlink()
-        
+        try:
+            data = json.loads(request.httprequest.data.decode('utf-8'))
+            
+            numero_empleado = data.get('numero_empleado')
+            
+            camarero = request.env['restaurante.camarero'].sudo().search([('numero_empleado', '=', numero_empleado)])
+            
+            camarero.sudo().unlink()
+        except:
+            return Response(
+                status=400,
+                content_type='application/json',
+                response=json.dumps({
+                    "error":"Bad Request",
+                    "message":"Datos invalidos"
+                })
+            )
--- controllers/mesa_controller.py
+++ controllers/mesa_controller.py
@@ -1,57 +1,100 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
+from odoo.http import Response
 import json
 
 class RestauranteMesaController(http.Controller):
     @http.route('/api/mesas', auth='public', type='json', methods=['GET'])
     def listMesas(self, **kw):
-        mesas = request.env['restaurante.mesa'].sudo().search([])
-        
-        jsonFinal = []
-        
-        for mesa in mesas:
-            jsonMesa = mesa.read(['numero', 'capacidad', 'estado'])[0]
+        try:
+            mesas = request.env['restaurante.mesa'].sudo().search([])
+            
+            jsonFinal = []
+            
+            for mesa in mesas:
+                jsonMesa = mesa.read(['numero', 'capacidad', 'estado'])[0]
 
-            if mesa.pedido_ids:
-                
-                jsonPedido = mesa.pedido_ids.read(['nombre_cliente', 'estado', 'total'])
-                jsonMesa['pedido_ids'] = jsonPedido
-                
-            else:
-                jsonMesa['pedido_ids'] = []
+                if mesa.pedido_ids:
+                    
+                    jsonPedido = mesa.pedido_ids.read(['nombre_cliente', 'estado', 'total'])
+                    jsonMesa['pedido_ids'] = jsonPedido
+                    
+                else:
+                    jsonMesa['pedido_ids'] = []
 
-            jsonFinal.append(jsonMesa)
+                jsonFinal.append(jsonMesa)
+            
+            return Response(
+                status=200,
+                content_type='application/json',
+                response=jsonFinal
+            ) 
         
-        return jsonFinal 
+        except:
+            return Response(
+                status=400,
+                content_type='application/json',
+                response=json.dumps({
+                    "error":"Bad Request",
+                    "message":"Datos invalidos"
+                })
+            )
     
     @http.route('/api/mesa/add', auth='public', type='json', methods=['PUT'])
     def addMesa(self, **kw):
-        
-        data = json.loads(request.httprequest.data.decode('utf-8'))
-        
-        request.env['restaurante.mesa'].sudo().create(data)
+        try:
+            data = json.loads(request.httprequest.data.decode('utf-8'))
+            
+            request.env['restaurante.mesa'].sudo().create(data)
+        except:
+            return Response(
+                status=400,
+                content_type='application/json',
+                response=json.dumps({
+                    "error":"Bad Request",
+                    "message":"Datos invalidos"
+                })
+            )
         
     
     @http.route('/api/mesa/update', auth='public', type='json', methods=['PATCH'])
     def updateMesa(self, **kw):
-        
-        data = json.loads(request.httprequest.data.decode('utf-8'))
+        try:
+            data = json.loads(request.httprequest.data.decode('utf-8'))
 
-        mesa_id = data.get('numero')
-        
-        mesa = request.env['restaurante.mesa'].sudo().search([('numero', '=', mesa_id)])
-        
-        data.pop('numero')
-        
-        mesa.sudo().write(data)
+            mesa_id = data.get('numero')
+            
+            mesa = request.env['restaurante.mesa'].sudo().search([('numero', '=', mesa_id)])
+            
+            data.pop('numero')
+            
+            mesa.sudo().write(data)
+        except:
+            return Response(
+                status=400,
+                content_type='application/json',
+                response=json.dumps({
+                    "error":"Bad Request",
+                    "message":"Datos invalidos"
+                })
+            )
 
     @http.route('/api/mesa/delete', auth='public', type='json', methods=['DELETE'])
     def deleteMesa(self, **kw):
-        data = json.loads(request.httprequest.data.decode('utf-8'))
-        mesa_id = data.get('numero')
-        
-        mesa = request.env['restaurante.mesa'].sudo().search([('numero', '=', mesa_id)])
-        
-        mesa.sudo().unlink()
-        
+        try:
+            data = json.loads(request.httprequest.data.decode('utf-8'))
+            mesa_id = data.get('numero')
+            
+            mesa = request.env['restaurante.mesa'].sudo().search([('numero', '=', mesa_id)])
+            
+            mesa.sudo().unlink()
+        except:
+            return Response(
+                status=400,
+                content_type='application/json',
+                response=json.dumps({
+                    "error":"Bad Request",
+                    "message":"Datos invalidos"
+                })
+            )
